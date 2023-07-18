f6971f3dabec5a36f3a1da89f1fed510f1262c93
muhd-kifayath <imkifayath@gmail.com>
2023-07-18 09:41:24 +0530

    Todo Recycler Updated

--- app/src/main/java/com/mk/trakit/TaskAdapter.java
+++ app/src/main/java/com/mk/trakit/TaskAdapter.java
@@ -1,24 +1,29 @@
 package com.mk.trakit;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.os.Bundle;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
@@ -35,22 +40,42 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
     @NonNull
     @Override
     public TaskAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.task_recycler, parent, false);
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.task_recycler_pending, parent, false);
         return new TaskAdapter.ViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, @SuppressLint("RecyclerView") int position) {
 
         final Task item = taskList.get(position);
-        holder.task.setText(item.getTask_name());
+        holder.title.setText(item.getTask_name());
+        if(!item.getDue_date().equals("")){
+            String date = item.getDue_date();
+            holder.due.setText(item.getDue_date());
+            try {
+                Date date1 = new SimpleDateFormat("dd-MM-yyyy").parse(date);
+                Date today = Calendar.getInstance().getTime();
+                if(date1.before(today)){
+                    holder.due.setTextColor(Color.RED);
+                }
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+            holder.due.setVisibility(View.VISIBLE);
+        }
+
         holder.task.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @SuppressLint("NotifyDataSetChanged")
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("TaskAssignments");
-                UserTask userTask = new UserTask(FirebaseAuth.getInstance().getCurrentUser().getUid(), isChecked);
-                ref.child(item.getId()).setValue(userTask);
+                String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                UserTask userTask = new UserTask(uid, isChecked);
+                ref.child(item.getId()).child(uid).setValue(userTask);
+                taskList.remove(position);
+                notifyDataSetChanged();
+                notifyItemRemoved(position);
+                ((ToDoActivity) context).refresh();
             }
         });
         holder.itemView.setOnClickListener(new View.OnClickListener() {
@@ -59,10 +84,6 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
                 Toast.makeText(context, "Task Clicked", Toast.LENGTH_SHORT).show();
             }
         });
-    }
-
-    private boolean toBoolean(int n) {
-        return n != 0;
     }
 
     @Override
@@ -88,10 +109,14 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         CheckBox task;
+        TextView title;
+        TextView due;
 
         ViewHolder(View view) {
             super(view);
             task = view.findViewById(R.id.todoCheckbox);
+            title = view.findViewById(R.id.todo_title);
+            due = view.findViewById(R.id.due_date);
         }
     }
 }
--- app/src/main/java/com/mk/trakit/ToDoActivity.java
+++ app/src/main/java/com/mk/trakit/ToDoActivity.java
@@ -3,35 +3,45 @@ package com.mk.trakit;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.DateFormat;
 import java.text.FieldPosition;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
 import java.util.UUID;
@@ -44,6 +54,9 @@ public class ToDoActivity extends AppCompatActivity {
     TextView cancel;
 
     String room_name, rid, imageUrl, uid;
+    private List<Task> pendingList, completedList;
+    private TaskAdapter pendingTaskAdapter;
+    private TaskCompletedAdapter completedTaskAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +71,24 @@ public class ToDoActivity extends AppCompatActivity {
         room_name = intent.getStringExtra("Title");
         rid = intent.getStringExtra("Id");
         imageUrl = intent.getStringExtra("Image");
+
+        ImageView dp = findViewById(R.id.image_room_profile);
+        Glide.with(ToDoActivity.this).load(imageUrl).circleCrop().into(dp);
+
+        TextView roomName = findViewById(R.id.roomName);
+        roomName.setText(room_name);
+
+        completedTasks.setLayoutManager(new LinearLayoutManager(this));
+        pendingTasks.setLayoutManager(new LinearLayoutManager(this));
+
+
+        completedList = new ArrayList<>();
+        pendingList = new ArrayList<>();
+        pendingTaskAdapter = new TaskAdapter(this, pendingList);
+        completedTaskAdapter = new TaskCompletedAdapter(this, completedList);
+
+        pendingTasks.setAdapter(pendingTaskAdapter);
+        completedTasks.setAdapter(completedTaskAdapter);
 
         uid = FirebaseAuth.getInstance().getUid();
 
@@ -95,7 +126,7 @@ public class ToDoActivity extends AppCompatActivity {
                         DatePickerDialog datePickerDialog=new DatePickerDialog(dialog.getContext(),R.style.DateTimeDialogTheme,onDateSetListener,calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
 
                         datePickerDialog.setTitle("Pick Due Date");
-                        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis()+86400000);
+                        //datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis()+86400000);
 
                         datePickerDialog.show();
 
@@ -112,7 +143,7 @@ public class ToDoActivity extends AppCompatActivity {
                         String due_date = duedate.getText().toString();
                         String desc = description.getText().toString();
 
-                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm");
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm aa");
                         Calendar calendar = Calendar.getInstance();
                         dateFormat.setTimeZone(TimeZone.getTimeZone("GMT+0530"));
                         String time_created = dateFormat.format(calendar.getTime());
@@ -127,9 +158,9 @@ public class ToDoActivity extends AppCompatActivity {
                             @Override
                             public void onSuccess(Void unused) {
                                 Toast.makeText(ToDoActivity.this, "Task Created!", Toast.LENGTH_SHORT).show();
-                                DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("TaskAssignments");
-                                UserTask userTask = new UserTask(FirebaseAuth.getInstance().getCurrentUser().getUid(),false);
-                                ref.child(tid).setValue(userTask);
+                                initializeTaskStatus(rid, tid);
+
+                                readTasks();
                                 dialog.dismiss();
                             }
                         });
@@ -150,5 +181,92 @@ public class ToDoActivity extends AppCompatActivity {
             }
         });
 
+        readTasks();
+
     }
+
+    private void initializeTaskStatus(String rid, String tid) {
+
+        DatabaseReference memberReference = FirebaseDatabase.getInstance().getReference().child("RoomMembers").child(rid);
+
+        memberReference.get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+            @Override
+            public void onSuccess(DataSnapshot dataSnapshot) {
+                for(DataSnapshot snap: dataSnapshot.getChildren()){
+                    UserTask user = snap.getValue(UserTask.class);
+                    String userId = user.getId();
+
+                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("TaskAssignments");
+                    UserTask userTask = new UserTask(userId,false);
+                    ref.child(tid).child(userId).setValue(userTask);
+                }
+            }
+        });
+
+    }
+
+    private void readTasks() {
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Tasks");
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("TaskAssignments");
+
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                pendingList.clear();
+                completedList.clear();
+
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    Task task = dataSnapshot.getValue(Task.class);
+
+                    String tid = task.getId();
+
+                    //pendingList.add(task);
+                    ref.child(tid).get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+                        @SuppressLint("NotifyDataSetChanged")
+                        @Override
+                        public void onSuccess(DataSnapshot data) {
+                            for(DataSnapshot snap: data.getChildren()){
+                                UserTask userTask = snap.getValue(UserTask.class);
+
+                                if(userTask.getId().equals(uid)){
+                                    if(userTask.isStatus()){
+                                        Log.d("task verified", "completed");
+                                        completedList.add(task);
+                                        Log.d("Completed List", completedList.toString());
+
+                                        completedTaskAdapter.notifyDataSetChanged();
+                                    }
+                                    else {
+                                        Log.d("task verified", "pending");
+                                        pendingList.add(task);
+                                        Log.d("Pending List", pendingList.toString());
+                                        pendingTaskAdapter.notifyDataSetChanged();
+
+                                    }
+                                    break;
+                                }
+                            }
+
+                        }
+
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void refresh(){
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(getIntent());
+        overridePendingTransition(0, 0);
+    }
+
 }
